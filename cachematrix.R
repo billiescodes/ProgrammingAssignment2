7f9409c737571c653db2c9eefa65a285b919925a
billiescodes <billie.rolih@gmail.com>
2015-06-21 16:04:02 -0700

    my makeCacheMatrix changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #initialize vector
+  m <- NULL
+  #set the value of the vector
+  set <- function(y) {
+    x <<- y
+    m <<-NULL
+  }
+  #get the value of the vetor
+  get <- function ()x  #gets value of vector
+  setMatrix <- function(solve) m <<- solve
+  getMatrix <- function() m # gets the value of the matrix
+  list( set=set, get=get,
+        setMatrix = setMatrix,
+        getMatrix = getMatrix)
 }
 
 
@@ -13,3 +26,4 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+w
