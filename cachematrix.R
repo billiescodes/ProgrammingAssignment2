0b82f11e155e73a97bbae7e2aba651cfaa6c5295
billiescodes <billie.rolih@gmail.com>
2015-06-21 16:17:23 -0700

    my second function changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Following function creates a special "matrix" object to cache 
+    ##the inverse of a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
   #initialize vector
@@ -21,9 +22,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function to solve the inverse of a matrix from the special "matrix" object
+    ## of the previous function. If the inverse has already been calculated 
+    ## (and the matrix has not changed), then the cachesolve
+        ##should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrix
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix,...)  #solves for the matrix
+  x$setMatrix(m)
+  m
 }
-w
+
